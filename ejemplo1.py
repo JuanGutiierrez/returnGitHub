57238e46697cb779ce56ce24fafb8afae8de46b4
carlanmonsal <309@academicos.cesde.edu.co>
2023-08-15 08:06:13 -0500

    feat: Creamos variables primitivas y listas de datos

---
+++ ejemplo1.py
@@ -0,0 +1,15 @@
+nombreUsuario = "Juanito"
+edadUsuario = 19
+estaturaUsuario = 1.67
+esHinchaDelVerde = True
+
+#comentario 
+'''Comentario de
+bloque'''
+
+print(f"Su nombre es: {nombreUsuario} y tiene {edadUsuario} años.")
+ 
+comidasFavoritas = ["Frisoles","Pastas","Papitas",
+                    "Sopa"]
+print(comidasFavoritas);
+print(comidasFavoritas[2])
