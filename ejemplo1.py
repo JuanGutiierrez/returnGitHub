7b7c1e827a5eebcd1a9620d1fc7f994e900eacab
carlanmonsal <309@academicos.cesde.edu.co>
2023-08-15 08:36:58 -0500

    feat: se reciben datos por teclado y se castean

--- ejemplo1.py
+++ ejemplo1.py
@@ -13,3 +13,14 @@ comidasFavoritas = ["Frisoles","Pastas","Papitas",
                     "Sopa"]
 print(comidasFavoritas);
 print(comidasFavoritas[2])
+
+
+#Entradas por teclado:
+
+lugarTrabajoUsuario = input(nombreUsuario+", Digite su lugar de trabajo: ")
+print(f"Señor {nombreUsuario}, usted trabaja en {lugarTrabajoUsuario}")
+
+numeroUno =int(input("Por favor digite el número uno: "))
+numeroDos = int(input("Por favor digite el número dos: "))
+suma = numeroDos + numeroUno
+print(f"Señor {nombreUsuario}, si suma el número {numeroUno} con el número {numeroDos} \n la suma de los dos numeros deberia de ser {suma}")
